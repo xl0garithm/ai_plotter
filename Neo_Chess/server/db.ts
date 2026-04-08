995c1985f39d1ac17099f1b5a064c71bd9625e38
Ty Byers <tybyers21@gmail.com>
2026-04-08 01:04:50 -0400

    consolidated code and added voice selection UI

--- Neo_Chess/server/db.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-import * as schema from "@shared/schema";
-
-const dbPath = process.env.DATABASE_URL || "./storage/neo_chess.db";
-
-const sqlite = new Database(dbPath);
-export const db = drizzle(sqlite, { schema });
